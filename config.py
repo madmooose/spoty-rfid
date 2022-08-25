56219a96a2d4c86e15300c9364e07012ff205f04
NIels Göttsch <niels@ziemlichoptimal.de>
2022-08-25 10:40:03 +0200

    check

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-CLIENT_ID: str = 'xxx'
-CLIENT_SECRET: str = 'xxx'
+CLIENT_ID: str = 'xxxx'
+CLIENT_SECRET: str = 'xxxx'
 REDIRECT_URI: str = 'http://localhost:8080'
 
-DEVICE_ID = 'xxx'
+DEVICE_ID = 'xxxx'
